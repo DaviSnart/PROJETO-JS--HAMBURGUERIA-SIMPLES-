3f25dd8c049200b88de5d238f150646b6470f034
DaviSnart <davi.soares9876543210@gmail.com>
2023-11-08 11:19:40 -0300

    nome da kmagem alterado

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ const menuOptions = [
     { name: 'X-Bacon', price: 32, vegan: false, src: './img/xbacon.png' },
     { name: 'X-Bacon Egg', price: 35, vegan: false, src: './img/bacon-egg.png' },
     { name: 'Monstruoso', price: 38, vegan: false, src: './img/monstruoso.png' },
-    { name: 'X-Vegan', price: 25, vegan: true, src: './img/xvegan (1).png' },
+    { name: 'X-Vegan', price: 25, vegan: true, src: './img/xvegan.png' },
     { name: 'X-Salada', price: 22, vegan: true, src: './img/xsalada.jpeg' },
     { name: 'X-Monstruoso Vegan', price: 35, vegan: true, src: './img/monstruoso-vegan.png' },
 ]
