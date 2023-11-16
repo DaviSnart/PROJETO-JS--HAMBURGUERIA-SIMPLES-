460446e65c9a1919e40f73bbf92cfcf85db9a2c5
DaviSnart <davi.soares9876543210@gmail.com>
2023-11-16 11:28:04 -0300

    adição de todas as funções dos botões do projeto.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
 const list = document.querySelector('ul')
 const buttonAll = document.querySelector('.button-all')
-let myLi = ''
+const activeButtonDiscount = document.querySelector('.discount-all')
+const activeSumPrices = document.querySelector('.sumValues')
+const openVegans = document.querySelector('.all-vegans')
 
 
-function showAll() {
-    menuOptions.forEach((product) => {
+
+function showAll(productsArray) {
+    let myLi = ''
+
+    productsArray.forEach((product) => {
         myLi += `
         <li>
             <img src=${product.src}>
@@ -17,4 +22,35 @@ function showAll() {
     list.innerHTML = myLi
 }
 
-buttonAll.addEventListener("click", showAll)
+function showDiscount() {
+    const newsPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price = product.price * 0.9,
+    }))
+
+    showAll(newsPrices)
+
+}
+
+function sumPrices() {
+    const result = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML =
+        `
+    <li>
+        <p>O valor total da compra é de R$ ${result}</p>
+    </li>
+    `
+}
+function showVegans() {
+    const itensVegans = menuOptions.filter(product => product.vegan)
+    showVegans(itensVegans)
+
+}
+
+buttonAll.addEventListener('click', () => showAll(menuOptions))
+activeButtonDiscount.addEventListener('click', showDiscount)
+activeSumPrices.addEventListener('click', sumPrices)
+openVegans.addEventListener('click', showVegans)
+
+
